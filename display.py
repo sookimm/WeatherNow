97e91cd7824196edc821cd8b3eabffd25c6c609c
sookimm <su241383@gmail.com>
2023-07-03 18:59:29 -0400

    without Flask

--- display.py
+++ display.py
@@ -1,10 +1,4 @@
-def display_weather(data):
-    if data["cod"] != "404":
-        temperature = data["main"]["temp"]
-        humidity = data["main"]["humidity"]
-        description = data["weather"][0]["description"]
-        print(f"Temperature: {temperature}°C")
-        print(f"Humidity: {humidity}%")
-        print(f"Description: {description}")
-    else:
-        print("City not found.")
+def display_weather(temperature, humidity, description):
+    print(f"Temperature: {temperature}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Description: {description}")
