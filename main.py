5a2bdc2b76cc3496ffd49656bd01b2309cd9bf0f
sookimm <su241383@gmail.com>
2023-07-03 19:18:54 -0400

    last change

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+from flask import Flask, render_template, request
 from weather import get_weather_info
-from display import display_weather
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
 def main():
-    api_key = "0ddf701347267e3e36de5e566221d650"
-    city = input("Enter city name: ")
-    weather_info = get_weather_info(api_key, city)
-    if weather_info:
-        temperature, humidity, description = weather_info
-        display_weather(temperature, humidity, description)
-    else:
-        print("City not found.")
+    if request.method == "POST":
+        api_key = "0ddf701347267e3e36de5e566221d650"
+        city = request.form.get("city")
+        weather_info = get_weather_info(api_key, city)
+        if weather_info:
+            temperature, humidity, description = weather_info
+            weather_data = {
+                "temperature": temperature,
+                "humidity": humidity,
+                "description": description
+            }
+            return render_template("index.html", weather=weather_data)
+        else:
+            return render_template("index.html", error="City not found.")
+    return render_template("index.html")
+
+def display_weather(temperature, humidity, description):
+    print(f"Temperature: {temperature}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Description: {description}")
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
+
